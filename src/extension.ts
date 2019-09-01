363d9383abb95e4eae2acd4588c4989d37a898e3
WalonLi <a63241924@gmail.com>
2019-09-01 15:09:39 +0800

    update dsc destination function.

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,26 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+
+class Edk2DscProvider implements vscode.DefinitionProvider {
+	provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Definition> {
+		/*
+		console.log('fileName: ' + document.uri.fsPath);
+		console.log('workDir: ' + path.dirname(document.uri.fsPath),);
+		console.log('word: ' + document.getText(document.getWordRangeAtPosition(position)));
+		console.log('line: ' + document.lineAt(position).text);
+		console.log(vscode.workspace.rootPath);
+		*/
+		let dest = document.lineAt(position).text.replace(new RegExp(/^\s*/g), '')
+												.replace(new RegExp(/[\s\{\}]*$/g), '')
+												.replace(new RegExp(/[a-zA-Z0-9\s]+\|/g), '');
+		dest = vscode.workspace.rootPath + '/' + dest;
+		if (fs.existsSync(dest)) {
+			return new vscode.Location(vscode.Uri.file(dest), new vscode.Position(0, 0));
+		}
+	}
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,6 +30,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "edk2-vscode" is now active!');
 
+	/*
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -21,6 +42,8 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+	*/
+	vscode.languages.registerDefinitionProvider({scheme: 'file', language: 'edk2_dsc'}, new Edk2DscProvider());
 }
 
 // this method is called when your extension is deactivated
